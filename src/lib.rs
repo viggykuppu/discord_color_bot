45e747827e32800ba58a382a8e4e5c6627367db2
viggykuppu <viggykuppu@gmail.com>
2020-02-12 14:16:40 -0500

    Adding basic functionality for creating color roles + skeleton of remainder of application

--- src/lib.rs
+++ src/lib.rs
@@ -41,19 +41,47 @@ fn ping(ctx: &mut Context, msg: &Message) -> CommandResult {
 
 #[command]
 fn color(ctx: &mut Context, msg: &Message) -> CommandResult {
-    let name = "test-role";
-    let colour = Colour::BLITZ_BLUE;
+    if user_has_existing_color_role() {
 
-    if let Some(guild_id) = msg.guild_id {
-        if let Err(e) = create_role(ctx, guild_id, name, colour) {
-            eprintln!("Error creating role: {}",e);
+    } else {
+        let name = &msg.author.name;
+        let role_name = format!("{}'s color", name);
+        let colour = parse_color(&msg.content);
+
+        if let Some(guild_id) = msg.guild_id {
+            if let Err(e) = create_role(&ctx, guild_id, &role_name, colour) {
+                eprintln!("Error creating role: {}",e);
+            }
         }
     }
+
+
+
+    // if let Some(_guild) = msg.guild(&ctx) {
+    //     let guild = _guild.read();
+    //     let guild_roles = &guild.roles;
+    //     if let Some(member) = &msg.member {
+    //         let member_roles = &member.roles;
+    //         for role_id in member_roles {
+    //             if let Some(guild_role) = &guild_roles.get(role_id) {
+
+    //             }
+    //         }
+    //     }
+    // }
     
     Ok(())
 }
 
-fn create_role(ctx: &mut Context, guild: GuildId, name: &str, colour: Colour) -> Result<(),Box<dyn Error>> {
+fn parse_color(msg: &str) -> Colour {
+    Colour::DARK_RED
+}
+
+fn user_has_existing_color_role() -> bool {
+    false
+}
+
+fn create_role(ctx: &&mut Context, guild: GuildId, name: &str, colour: Colour) -> Result<(),Box<dyn Error>> {
     guild.create_role(ctx, |r| r.hoist(true).name(name).colour(colour.0 as u64))?;
 
     Ok(())
