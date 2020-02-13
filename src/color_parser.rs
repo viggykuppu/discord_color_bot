80aa152b5976954babca6d3c043a45ec39cfdda6
Vignesh Kuppusamy <vkuppusamy@techdynamism.com>
2020-02-12 21:19:45 -0500

    Adding basic hex color parsing

--- src/color_parser.rs
+++ src/color_parser.rs
@@ -1,12 +1,14 @@
 use serenity::utils::Colour;
+use hex;
 
-pub fn parse_color(msg: &str) -> Colour {
-    let color_arg = parse_color_arg(msg);
+pub fn parse_color(msg: &str) -> Colour {   
     match parse_color_arg(msg) {
         Some(color_arg) => {
-            
+            if let Some(color) = parse_hex_color(color_arg) {
+                return color;
+            }
         },
-        None => eprintln!("Not enough arguments!");
+        None => eprintln!("Not enough arguments!")
     }
     Colour::DARK_RED
 }
@@ -17,3 +19,20 @@ fn parse_color_arg(msg: &str) -> Option<&str> {
     chunks.next();
     return chunks.next();
 }
+
+fn parse_hex_color(color_arg: &str) -> Option<Colour> {
+    let mut hex_arg = color_arg;
+    if color_arg.starts_with("#") {
+        hex_arg = &color_arg[1..];
+    }
+    match hex::decode(hex_arg) {
+        Ok(hex) => {
+            if hex.len() == 3 {
+                return Some(Colour::from_rgb(hex[0], hex[1], hex[2]));
+            }
+        },
+        Err(e) => { eprintln!("Error parsing supplied hex color: {}",e) }
+    }
+    None
+}
+
